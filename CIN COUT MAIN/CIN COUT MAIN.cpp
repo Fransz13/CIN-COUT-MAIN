e21767b68908bafce53ccd02f03d88e1dd41fe50
Fransz13 <202410383@fit.edu.ph>
2024-11-22 10:43:17 +0800

    Add project files.

---
+++ CIN COUT MAIN/CIN COUT MAIN.cpp
@@ -0,0 +1,19 @@
+// CIN COUT MAIN.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    // Asking the user to input a number
+    cout << "Enter a number: ";
+    cin >> number;
+
+    // Displaying the entered number
+    cout << "You entered: " << number << endl;
+
+    return 0;
+}
+
